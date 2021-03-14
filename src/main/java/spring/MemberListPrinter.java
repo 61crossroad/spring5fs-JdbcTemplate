bfac18bb880245a80b819ac0ef71e62537dd30ee
Shawn Kim <shawnkim@Shawnui-MacBookPro.local>
2021-03-14 21:39:08 +0900

    create base classes

--- src/main/java/spring/MemberListPrinter.java
+++ src/main/java/spring/MemberListPrinter.java
@@ -1,2 +1,18 @@
-package spring;public class MemberListPrinter {
+package spring;
+
+import java.util.Collection;
+
+public class MemberListPrinter {
+    private MemberDao memberDao;
+    private MemberPrinter printer;
+
+    public MemberListPrinter(MemberDao memberDao, MemberPrinter printer) {
+        this.memberDao = memberDao;
+        this.printer = printer;
+    }
+
+    public void printAll() {
+        Collection<Member> members = memberDao.selectAll();
+        members.forEach(m -> printer.print(m));
+    }
 }
